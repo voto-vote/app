1094f2c38bcfb7d540421d92b466a37205f01d5d
Josua Frank <frank.josua@gmail.com>
2025-03-10 17:33:14 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./(header)/header";
 
-const inter = Inter();
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Voto",
