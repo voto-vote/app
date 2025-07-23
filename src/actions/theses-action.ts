18935cff266e97a843d0af715a023201a92d07cc
Josua Frank <frank.josua@gmail.com>
2025-07-23 15:54:00 +0200

    Added thesis explanations

--- src/actions/theses-action.ts
+++ src/actions/theses-action.ts
@@ -3,7 +3,7 @@
 import { eq, and } from "drizzle-orm";
 import { db } from "@/db/drizzle";
 import { statements, statementTranslations } from "@/db/schema";
-import { Theses } from "@/types/theses";
+import { Theses, Thesis } from "@/types/theses";
 
 export async function getTheses(
   instanceId: string,
@@ -28,13 +28,48 @@ export async function getTheses(
     );
 
   // Transform the result into the desired format
-  const theses: Theses = result.map((row) => ({
-    id: row.statementId.toString(),
-    category: row.title.toString(),
-    text: row.text,
-    explanations: [], //TODO
-    additionalInfos: "", //TODO
-  }));
+  const theses: Theses = result.map((row) => {
+    const { text, explanations } = parseThesisText(
+      row.text,
+      "titleTODO",
+      "locationTODO"
+    );
+    return {
+      id: row.statementId.toString(),
+      category: row.title.toString(),
+      text,
+      explanations,
+      additionalInfos: "", //TODO
+    };
+  });
 
   return theses;
+}
+
+export function parseThesisText(
+  text: string,
+  electionTitle: string,
+  electionLocation: string
+): Pick<Thesis, "text" | "explanations"> {
+  text = text
+    .replaceAll("{title}", electionTitle)
+    .replaceAll("{location}", electionLocation);
+
+  const matches = text.matchAll(/\((.*?)\)\[(.*?)\]/g);
+  const explanations: Thesis["explanations"] = [];
+
+  for (const match of matches) {
+    if (match[1] && match[2]) {
+      // Replace the matched text with the first group (the thesis text)
+      text = text.replace(match[0], match[1].trim());
+      // Store the explanation with its start and end offsets
+      explanations.push({
+        text: match[2].trim(),
+        startOffset: match.index,
+        endOffset: match.index + match[1].trim().length,
+      });
+    }
+  }
+
+  return { text, explanations };
 }
