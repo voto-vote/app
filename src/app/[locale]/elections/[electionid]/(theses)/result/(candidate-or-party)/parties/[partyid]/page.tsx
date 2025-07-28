90e553963ec7d6b03d6367d53b6f276688200c8b
Josua Frank <frank.josua@gmail.com>
2025-07-28 12:27:33 +0200

    translations...

--- src/app/[locale]/elections/[electionid]/(theses)/result/(candidate-or-party)/parties/[partyid]/page.tsx
+++ src/app/[locale]/elections/[electionid]/(theses)/result/(candidate-or-party)/parties/[partyid]/page.tsx
@@ -1,35 +1,8 @@
 "use client";
 
-import MatchBar from "@/app/[locale]/elections/[electionid]/(theses)/result/match-bar";
-import { useEffect, useState } from "react";
-import { useBackButtonStore } from "@/stores/back-button-store";
-import { useThesesStore } from "@/stores/theses-store";
-import { useRatingsStore } from "@/stores/ratings-store";
-import { useElection } from "@/contexts/election-context";
-import ThesesResultCarousel from "../../../theses-result-carousel";
-import { Bookmark } from "@/components/icons/bookmark";
-import LegendBottomBar from "../../../legend-bottom-bar";
 import CandidateOrParty from "../../candidate-or-party";
 
 export default function CandidatePage() {
-  const [isBookmarked, setIsBookmarked] = useState(false);
-  const { election } = useElection();
-  const { theses } = useThesesStore();
-  const { ratings, setRating, setFavorite } = useRatingsStore();
-  const { setBackPath } = useBackButtonStore();
-
-  useEffect(() => {
-    if (election?.id) {
-      setBackPath(`/elections/${election?.id}/result`);
-    } else {
-      setBackPath("/");
-    }
-  }, [election?.id, setBackPath]);
-
-  if (!theses) {
-    return null;
-  }
-
   return (
     <CandidateOrParty
       participant={{
--- src/app/[locale]/elections/[electionid]/(theses)/result/result-list.tsx
+++ src/app/[locale]/elections/[electionid]/(theses)/result/result-list.tsx
@@ -13,6 +13,8 @@ import { useRouter } from "@/i18n/navigation";
 import { useElection } from "@/contexts/election-context";
 import BottomBar from "./bottom-bar";
 import { useBookmarkStore } from "@/stores/bookmark-store";
+import { useTranslations } from "next-intl";
+import { useThesesStore } from "@/stores/theses-store";
 
 interface ResultListProps {
   filterBookmarked: boolean;
@@ -26,9 +28,15 @@ export default function ResultList({
   const [tab, setTab] = useState<"candidates" | "parties">("candidates");
   const [filterOpen, setFilterOpen] = useState(false);
   const { election } = useElection();
+  const { theses } = useThesesStore();
   const { ratings } = useRatingsStore();
   const { bookmarks, toggleCandidate, toggleParty } = useBookmarkStore();
   const router = useRouter();
+  const t = useTranslations("ResultList");
+
+  if (!theses) {
+    return null;
+  }
 
   return (
     <div className="container mx-auto max-w-3xl p-2 pb-13">
@@ -40,13 +48,14 @@ export default function ResultList({
           transition={{ duration: 0.5 }}
         >
           <p className="text-sm md:max-w-1/2">
-            Auf Basis Deiner{" "}
-            {Object.values(ratings[election.id] ?? {}).reduce(
-              (n, r) => (r.rating ? n + 1 : n),
-              0
-            )}{" "}
-            beantworteten Thesen zur Wahl {election.title} {election.subtitle}{" "}
-            {new Date(election.electionDate).getFullYear()}
+            {t("explanation", {
+              count: Object.values(ratings[election.id] ?? {}).reduce(
+                (n, r) => ((r.rating ?? 0 > 0) ? n + 1 : n),
+                0
+              ),
+              electionTitle: election.title,
+              electionSubtitle: election.subtitle,
+            })}
           </p>
         </motion.div>
 
@@ -58,14 +67,14 @@ export default function ResultList({
               className={`text-lg transition-all
                 ${tab === "candidates" ? "text-primary font-semibold" : "text-primary/70"}`}
             >
-              Kandidierende
+              {t("candidatesTab")}
             </TabsTrigger>
             <TabsTrigger
               value="parties"
               className={`text-lg transition-all
                 ${tab === "parties" ? "text-primary font-semibold" : "text-primary/70"}`}
             >
-              Parteien
+              {t("partiesTab")}
             </TabsTrigger>
           </TabsList>
         </Tabs>
@@ -100,7 +109,7 @@ export default function ResultList({
       {/* Bottom Bar */}
       <BottomBar>
         <Button variant="ghost" onClick={() => setFilterOpen(true)}>
-          Filter
+          {t("filterButton")}
         </Button>
         <Button
           variant="ghost"
@@ -111,14 +120,20 @@ export default function ResultList({
           }
           onClick={() => setFilterBookmarked(!filterBookmarked)}
         >
-          Merkliste (
-          {(bookmarks[election.id]?.parties?.length ?? 0) +
-            (bookmarks[election.id]?.candidates?.length ?? 0)}
-          )
+          {t("bookmarksButton", {
+            count:
+              (bookmarks[election.id]?.parties?.length ?? 0) +
+              (bookmarks[election.id]?.candidates?.length ?? 0),
+          })}
         </Button>
       </BottomBar>
 
-      <FilterDialog open={filterOpen} onOpenChange={setFilterOpen} />
+      <FilterDialog
+        election={election}
+        theses={theses}
+        open={filterOpen}
+        onOpenChange={setFilterOpen}
+      />
     </div>
   );
 }
