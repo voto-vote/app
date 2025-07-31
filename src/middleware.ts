1504a0c209aa4fb2d72b38a8c72061a246f2ae3e
Josua Frank <frank.josua@gmail.com>
2025-07-31 10:03:15 +0200

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,45 +7,52 @@ export const runtime = "nodejs";
 
 // If an election does not support the requested locale, we redirect to the default locale
 export default async function middleware(request: NextRequest) {
-  console.log("Middleware triggered for:", request.nextUrl.toString());
-  // "" / ...
   const [, ...segments] = request.nextUrl.pathname.split("/");
-  let handleI18nRouting;
+  let handleI18nRouting = createMiddleware(routing);
 
   const electionsPathIndex = segments.indexOf("elections");
   if (electionsPathIndex !== -1 && electionsPathIndex < segments.length - 1) {
-    const staticLocales = routing.locales;
     const electionId = segments[electionsPathIndex + 1];
     const election = await getElection(electionId);
-    const electionLocales =
-      election?.locales?.map((l) => l.split("-")[0]) || [];
-    // Intersection of the static locales and the election's locales
-    const supportedLocales = staticLocales.filter((l) =>
-      electionLocales.includes(l)
-    );
 
-    handleI18nRouting = createMiddleware({
-      locales: supportedLocales,
-      defaultLocale: routing.defaultLocale,
-    });
+    if (election) {
+      const supportedLocales = routing.locales;
+      const electionLocales =
+        election.locales.map((l) => l.split("-")[0]) || [];
+      // Intersection of the static locales and the election's locales
+      const supportedElectionLocales = supportedLocales.filter((l) =>
+        electionLocales.includes(l)
+      );
+      if (supportedElectionLocales.length === 0) {
+        throw new Error(
+          `The election ${election.title} - ${election.subtitle} (${electionId}) has unsupported locales. Supported locales: [${supportedLocales.join(
+            ", "
+          )}]. Election locales: [${electionLocales.join(", ")}].`
+        );
+      }
 
-    // If the url already starts with a locale that is not in the supported locales, we need to remove it
-    // This would otherwise lead to a redirect loop
-    if (electionsPathIndex > 0) {
-      const locale = segments[electionsPathIndex - 1];
-      if (
-        !supportedLocales.includes(locale as (typeof routing.locales)[number])
-      ) {
-        request.nextUrl.pathname =
-          "/" + segments.slice(electionsPathIndex).join("/");
+      handleI18nRouting = createMiddleware({
+        locales: supportedElectionLocales,
+        defaultLocale: routing.defaultLocale,
+      });
+
+      // If the url already starts with a locale that is not in the supported locales, we need to remove it
+      // The next-intl middleware will not see this path as a valid locale and will just append the default locale
+      // Wrong behavior: /ru/elections/1234 -> /de/ru/elections/1234
+      // Correct behavior: /ru/elections/1234 -> /de/elections/1234
+      if (electionsPathIndex > 0) {
+        const locale = segments[electionsPathIndex - 1];
+        if (
+          !supportedElectionLocales.includes(locale as (typeof routing.locales)[number])
+        ) {
+          request.nextUrl.pathname =
+            "/" + segments.slice(electionsPathIndex).join("/");
+        }
       }
     }
-  } else {
-    handleI18nRouting = createMiddleware(routing);
   }
 
-  const response = handleI18nRouting(request);
-  console.log("Redirecting to: ", response.status === 307 ? response.headers.get("Location") : request.nextUrl.toString());
+  return handleI18nRouting(request);
 }
 
 export const config = {
