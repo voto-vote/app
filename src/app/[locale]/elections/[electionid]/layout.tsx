b1f2a4d9cea550fc30554cf0fe8ce4ac8ecc89eb
Josua Frank <frank.josua@gmail.com>
2025-08-23 08:38:28 +0200

    better google result

--- src/app/[locale]/elections/[electionid]/layout.tsx
+++ src/app/[locale]/elections/[electionid]/layout.tsx
@@ -2,6 +2,29 @@ import { getElection } from "@/actions/election-action";
 import { ElectionProvider } from "@/contexts/election-context";
 import ElectionChangeHandler from "./election-change-handler";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ electionid: string }>;
+}): Promise<Metadata> {
+  const { electionid } = await params;
+  const election = await getElection(electionid);
+  if (!election) {
+    notFound();
+  }
+
+  let description = election.description;
+  if (description.length > 150) {
+    description = description.slice(0, 150) + "...";
+  }
+
+  return {
+    title: election.title + " " + election.subtitle + " | VOTO",
+    description,
+  };
+}
 
 export default async function ElectionLayout({
   children,
